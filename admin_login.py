c58edb4565cbab664d90082c0c6b27293c11ff4f
Admin <kkrauza@o2.pl>
2021-01-20 22:01:23 +0100

    login

---
+++ admin_login.py
@@ -0,0 +1,39 @@
+import tkinter as tk
+from functools import partial
+import psycopg2
+def adminpanel():
+    root=tk.Tk()
+    root.mainloop()
+def validate(login, password):
+    login = logent.get()
+    password = passent.get()
+    print(login, password)
+    try:
+        conn = psycopg2.connect(user=login, password=password, dbname='postgres', host='localhost')
+        val = True
+        root.destroy()
+        adminpanel()
+
+    except:
+        print('inicorret')
+        val = False
+    return val
+
+root = tk.Tk()
+root.title('Login')
+loginlab = tk.Label(root, text='Login:')
+loginlab.grid(column=0, row=0)
+passlab = tk.Label(root, text='Password:')
+passlab.grid(column=0, row=1)
+login = ''
+logent = tk.Entry(root, show='*', textvariable=login)
+logent.grid(column=1, row=0)
+password=''
+passent = tk.Entry(root, show='*', textvariable=password)
+passent.grid(column=1, row=1)
+validate = partial(validate, login, password)
+loginbut=tk.Button(text='Login', command=validate)
+loginbut.grid(column=0, row=2)
+exitbut=tk.Button(root, text='EXIT', command=quit)
+exitbut.grid(column=1, row=2)
+root.mainloop()
